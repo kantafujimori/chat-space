e425b1542ed048616be1ac0412e151be3d6e3557
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2018-12-23 19:37:15 +0900

    Edit mesages_controller.rb
    メッセージコントローラーのモデルのテストの編集をした

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,8 +58,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   #〜省略〜
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
+  #〜省略〜
   end
-end
+
